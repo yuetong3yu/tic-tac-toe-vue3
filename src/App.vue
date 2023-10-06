8ff96bca69c166c219137a6280834967f92218ee
Chris Yu <yuetong3yu@gmail.com>
2023-10-06 22:39:41 +1100

    remove env config files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>Hello Vue3, {{ VITE_SUPER_KEY }}</div>
+  <div>Hello Vue3</div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const VITE_SUPER_KEY = ref(import.meta.env.VITE_SUPER_KEY)
-</script>
+<script setup lang="ts"></script>
