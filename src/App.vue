8f68e6f10bcffc68838e1b4d20159eecdcd96669
Chris Yu <yuetong3yu@gmail.com>
2023-10-06 23:03:33 +1100

    draw title & checkerboard

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="font-bold text-red-900">Hello Vue3</div>
+  <div
+    class="flex flex-col items-center justify-center bg-slate-400 w-screen h-screen"
+  >
+    <section class="font-bold text-3xl mb-4 italic text-green-400">
+      Tic Tac Toe
+    </section>
+    <CheckerBoard />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CheckerBoard from './components/CheckerBoard.vue'
+</script>
---
+++ src/components/CheckerBoard.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="flex w-96 h-96 bg-slate-600 border-2 border-black"></div>
+</template>
