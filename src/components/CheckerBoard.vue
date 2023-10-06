37174194f8b69f209cce41a59e908274b9715b6c
Chris Yu <yuetong3yu@gmail.com>
2023-10-06 23:11:20 +1100

    draw lines in checkerboard

--- src/components/CheckerBoard.vue
+++ src/components/CheckerBoard.vue
@@ -1,3 +1,31 @@
 <template>
-  <div class="flex w-96 h-96 bg-slate-600 border-2 border-black"></div>
+  <div class="flex flex-col w-96 h-96 bg-slate-600 border-2 border-black">
+    <!-- top layer -->
+    <div class="flex">
+      <!-- top left cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top middle cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top right cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+    </div>
+    <!-- middle layer -->
+    <div class="flex">
+      <!-- top left cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top middle cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top right cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+    </div>
+    <!-- bottom layer -->
+    <div class="flex">
+      <!-- top left cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top middle cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+      <!-- top right cell -->
+      <div class="border-b border-r border-black w-32 h-32"></div>
+    </div>
+  </div>
 </template>
