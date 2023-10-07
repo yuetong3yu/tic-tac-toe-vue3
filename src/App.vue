2981072d126f977edccb292be7ed461f39453821
Chris Yu <yuetong3yu@gmail.com>
2023-10-07 22:20:59 +1100

    change title according to who win

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@
     class="flex flex-col items-center justify-center bg-slate-400 w-screen h-screen"
   >
     <section class="font-bold text-4xl mb-4 italic text-green-400">
-      Tic Tac Toe
+      {{ winnerStr.length ? `${winnerStr} won!` : 'Tic Tac Toe' }}
     </section>
     <CheckerBoard @winner="winnerCB" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import CheckerBoard from './components/CheckerBoard.vue'
 
+const winnerStr = ref<string>('')
 const winnerCB = (winner: string) => {
-  console.log('winner is: ', winner)
+  if (winner.length) winnerStr.value = winner === '0' ? 'Circle' : 'Cross'
 }
 </script>
