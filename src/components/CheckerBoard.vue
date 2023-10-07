24bcc3794d367492381f8493b965da4f325596c0
Chris Yu <yuetong3yu@gmail.com>
2023-10-07 22:11:47 +1100

    win logic

--- src/components/CheckerBoard.vue
+++ src/components/CheckerBoard.vue
@@ -22,18 +22,86 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import HorizontalLine from './HorizontalLine.vue'
 import VerticalLine from './VerticalLine.vue'
 import Cross from './Cross.vue'
 import Circle from './Circle.vue'
 
-const checkerBoardState = ref([
+const checkerBoardState = ref<(null | number)[][]>([
   new Array(3).fill(null),
   new Array(3).fill(null),
   new Array(3).fill(null),
 ])
+const constructedString = computed(() => {
+  let res = ''
+  for (const cells of checkerBoardState.value) {
+    for (const cell of cells) {
+      res += cell === null ? '?' : String(cell)
+    }
+  }
+  return res
+})
 const currentPlayerState = ref(0)
+const whoWin = computed(() => {
+  if (
+    constructedString.value[0] !== '?' &&
+    constructedString.value[0] === constructedString.value[1] &&
+    constructedString.value[0] === constructedString.value[2]
+  ) {
+    return constructedString.value[0]
+  }
+  if (
+    constructedString.value[0] !== '?' &&
+    constructedString.value[0] === constructedString.value[3] &&
+    constructedString.value[0] === constructedString.value[6]
+  ) {
+    return constructedString.value[0]
+  }
+  if (
+    constructedString.value[0] !== '?' &&
+    constructedString.value[0] === constructedString.value[4] &&
+    constructedString.value[0] === constructedString.value[8]
+  ) {
+    return constructedString.value[0]
+  }
+  if (
+    constructedString.value[1] !== '?' &&
+    constructedString.value[1] === constructedString.value[4] &&
+    constructedString.value[1] === constructedString.value[7]
+  ) {
+    return constructedString.value[1]
+  }
+  if (
+    constructedString.value[2] !== '?' &&
+    constructedString.value[2] === constructedString.value[5] &&
+    constructedString.value[2] === constructedString.value[8]
+  ) {
+    return constructedString.value[2]
+  }
+  if (
+    constructedString.value[2] !== '?' &&
+    constructedString.value[2] === constructedString.value[4] &&
+    constructedString.value[2] === constructedString.value[7]
+  ) {
+    return constructedString.value[2]
+  }
+  if (
+    constructedString.value[3] !== '?' &&
+    constructedString.value[3] === constructedString.value[4] &&
+    constructedString.value[3] === constructedString.value[5]
+  ) {
+    return constructedString.value[3]
+  }
+  if (
+    constructedString.value[6] !== '?' &&
+    constructedString.value[6] === constructedString.value[7] &&
+    constructedString.value[6] === constructedString.value[8]
+  ) {
+    return constructedString.value[6]
+  }
+  return null
+})
 
 const onCellClick = (position: [number, number]) => {
   if (checkerBoardState.value[position[0]][position[1]] !== null) return
