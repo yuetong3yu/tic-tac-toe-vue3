8bf35c63a31416727dc18a0d8d54822978641d8e
Chris Yu <yuetong3yu@gmail.com>
2023-10-08 10:19:30 +1100

    add restart button

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <div
     class="flex flex-col items-center justify-center bg-slate-400 w-screen h-screen"
   >
-    <section class="font-bold text-4xl mb-4 italic text-green-400">
+    <section
+      class="flex items-center font-bold text-4xl mb-4 italic text-green-400"
+    >
       {{ winnerStr.length ? `${winnerStr} won!` : 'Tic Tac Toe' }}
+      <Restart v-if="!!winnerStr" @onclick="onRestart" />
     </section>
     <CheckerBoard @winner="winnerCB" />
   </div>
@@ -12,9 +15,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import CheckerBoard from './components/CheckerBoard.vue'
+import Restart from './components/Restart.vue'
 
 const winnerStr = ref<string>('')
 const winnerCB = (winner: string) => {
   if (winner.length) winnerStr.value = winner === '0' ? 'Circle' : 'Cross'
+}
+const onRestart = () => {
+  console.log('1234 restart')
 }
 </script>
---
+++ src/components/Restart.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="text-lg p-2 border-red-500 border rounded-lg ml-4 text-red-200 hover:scale-110 transition-all"
+    @click="onButtonClick"
+  >
+    Restart
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['onclick'])
+
+const onButtonClick = () => emit('onclick')
+</script>
